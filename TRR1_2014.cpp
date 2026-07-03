01b68c5b6b39a893a0e681651fdd28050784c0cc
Bùi Phan Minh Nhật <buiphanm@gmail.com>
2026-07-03 21:25:42 +0700

    Refactor DP logic

--- TRR1_2014.cpp
+++ TRR1_2014.cpp
@@ -1,35 +1,35 @@
+//author: nhatdevzizi
+
+/* Phiêu du dòng code, mộng đời trai
+Nhật gửi lòng mình giữa sớm mai.
+Chẳng ngại sương mờ vây thuật toán
+Vất vả một thuở, rạng tương lai. */
+
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define boost ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+const int MOD = 1e9 + 7;
 
-using ll = long long;
-void solve() {
-    int n, k;
-    if (!(cin >> n >> k)) return;
-    ll total = 1LL << n;
-    vector<vector<ll>> dp(n + 1, vector<ll>(k, 0));
-    dp[1][0] = 1; 
-    if (k > 1) {
-        dp[1][1] = 1; 
-    }
-    for (int i = 2; i <= n; ++i) {
-        for (int j = 0; j < k; ++j) {
-            dp[i][0] += dp[i - 1][j];
-        }
-        for (int j = 1; j < k; ++j) {
-            dp[i][j] = dp[i - 1][j - 1];
-        }
-    }
-    ll not_contains = 0;
-    for (int j = 0; j < k; ++j) {
-        not_contains += dp[n][j];
-    }
-    cout << total - not_contains << endl;
-}
+ll arr[1000005];
+ll n, k, a, b, x, y, l, r;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    solve();
+int32_t main(){
+    boost;
+    cin >> n >> k;
+    vector<ll> dp(n+2,0);
+    for(int i = 1; i <= n; ++i){
+        if(i < k) dp[i] = (1LL << i);
+        else if(i == k) dp[i] = ((1LL << i)-1);
+        else {
+            for(int j = 1; j <= k; ++j){
+                dp[i] += dp[i-j];
+            }
+        }
+    }
+    cout << ((1LL << n) - dp[n]) << endl;
     return 0;
 }
